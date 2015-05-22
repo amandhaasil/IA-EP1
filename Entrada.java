a04c52a56376dae4d3bc11519db11bdc4eb0ee0d
Hawkiz <cinthia.valeije@usp.br>
2015-05-22 18:18:35 -0300

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -1,13 +1,17 @@
-//package ep.ia;
+package ep.ia;
 /*Objeto de Entrada que possui um array de linhas que contém os 64 valores das respectivas colunas e
- * o resultado de cada linha que representa o valor escrito a mao pela pessoa.
+ * o resultado de cada linha que representa o digito escrito a mao pela pessoa.
  */
 public class Entrada {
-	public double[] linha;
-	public int resultado;
+	public double[] linha;//linha contendo todos os atributos por entrada
+	public int resultado;//contem a classe da linha correspondente
 	
 	public Entrada(double[] linha, int resultado){
 		this.linha = linha;
 		this.resultado = resultado;
 	}
+	
+	
+	
+
 }
